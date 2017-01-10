b62c409c4b650ddeeb5575ed2bd22e5b03166637
bfayers <ben.fayers@gmail.com>
2017-01-10 23:04:41 +0000

    First commit of PXT Vagrantfile and provison, WIP, DOES NOT WORK

---
+++ PXT/provision.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# This script is run by Vagrant when a new machine is provisioned.
+#
+ 
+sudo apt-get -y install git
+
+sudo apt-get -y install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get update
+sudo apt-get -y install nodejs
+
+cd /pxt && rm -r * && rm -r .* && git clone https://github.com/microsoft/pxt .
+git clone https://github.com/microsoft/pxt-microbit
+cd /pxt
+
+sudo npm install -g jake
+sudo npm install -g typings
+npm install
+typings install
+jake
+sudo npm install -g pxt
+
+cd pxt-microbit
+sudo pxt serve
