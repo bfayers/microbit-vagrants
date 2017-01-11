49137c3a16ae7c3106c3df9a39c3aa7d29db6fea
Owen Brotherwood <OwenBrotherwood@users.noreply.github.com>
2017-01-12 00:51:57 +0100

    Doc grub problem
    plus some other stuff
    Great commit comment :)

--- PXT/provision.sh
+++ PXT/provision.sh
@@ -5,8 +5,8 @@
 echo initial-apt-get 
 {
   sudo apt-get update
-  sudo apt-get dist-upgrade
   sudo apt-get -y install xauth xterm git
+# sudo apt-get -y dist-upgrade #TODO:Solve grub problem http://askubuntu.com/questions/325872/ubuntu-unattended-apt-get-upgrade-grub-install-dialog
 }>/tmp/initial-apt-get-log
 
 cat <<EOF
