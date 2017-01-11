31391cc97b5eb482a509149b9ce06d8b60edf475
bfayers <ben.fayers@gmail.com>
2017-01-11 20:16:50 +0000

    Move some things around, more npm. Should work.

--- PXT/provision.sh
+++ PXT/provision.sh
@@ -12,6 +12,11 @@ sudo apt-get -y install nodejs
 sudo apt-get -y install firefox
 sudo apt-get -y install libgl1-mesa-glx
 
+cd /home/vagrant/ && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get -y install -f
+#Not the prettiest way of doing this ^ but it works.
+
 cd /home/vagrant/pxt && rm -r * && rm -r .* 
 cd /home/vagrant/pxt && git clone https://github.com/microsoft/pxt .
 git clone https://github.com/microsoft/pxt-microbit
@@ -23,6 +28,9 @@ sudo npm install
 sudo typings install
 sudo jake
 sudo npm install -g pxt
+sudo pxt target microbit
 
 cd pxt-microbit
-sudo pxt serve
+sudo npm install -g pxt
+sudo npm install
+pxt serve
