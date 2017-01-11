8a381be17770b0d9fd17d043cb1a3b9dd1045ed6
Owen Brotherwood <OwenBrotherwood@users.noreply.github.com>
2017-01-12 00:04:32 +0100

    initial-apt-get
    comment out rest

--- PXT/provision.sh
+++ PXT/provision.sh
@@ -2,16 +2,20 @@
 #
 # This script is run by Vagrant when a new machine is provisioned.
 #
-sudo apt-get update
-sudo apt-get -y install git
+echo initial-apt-get 
+{
+  sudo apt-get update
+  sudo apt-get dist-upgrade
+  sudo apt-get -y install xauth xterm git
+}>/tmp/initial-apt-get-log
 
-sudo apt-get -y install python-software-properties
+cat <<EOF
+# The following is under investigation
+sudo apt-get --qq -y install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get update
 sudo apt-get -y install nodejs
 sudo apt-get -y install firefox
 sudo apt-get -y install libgl1-mesa-glx
-sudo apt-get install xauth
 
 cd /home/vagrant/pxt && rm -r * && rm -r .* 
 cd /home/vagrant/pxt && git clone https://github.com/microsoft/pxt .
@@ -30,3 +34,4 @@ cd pxt-microbit
 sudo npm install -g pxt
 sudo npm install
 pxt serve
+EOF
