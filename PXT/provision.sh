6bfb26def147143c57285dfe55fc09b3cf4de959
Owen Brotherwood <OwenBrotherwood@users.noreply.github.com>
2017-01-12 14:22:44 +0100

    Update provision.sh

--- PXT/provision.sh
+++ PXT/provision.sh
@@ -3,19 +3,27 @@
 # This script is run by Vagrant when a new machine is provisioned.
 #
 
-echo debug-info-log
+echo start-log
 {
   whoami
   who am i
   set
-} >>/tmp/vagrant-debug-info-log 2>>&1
+} >>/tmp/start-log 2>&1
+
+echo allow-quick-ssh
+{
+  runuser - vagrant -c '
+    mkdir ~vagrant/local; 
+    echo export PATH=~vagrant/local/bin:$PATH >>~vagrant/.bashrc;
+    '
+} >>/tmp/allow-quick-ssh-log 2>&1
 
 echo initial-apt-get 
 {
   sudo apt-get update
   apt-get -y install git build-essential  
 # sudo apt-get -y dist-upgrade #TODO:Solve grub problem http://askubuntu.com/questions/325872/ubuntu-unattended-apt-get-upgrade-grub-install-dialog
-} >>/tmp/vagrant-debug-info-log 2>>&1
+} >>/tmp/vagrant-debug-info-log 2>&1
 
 echo home-local-node-install #https://gist.github.com/isaacs/579814
 {
@@ -25,15 +33,15 @@ echo home-local-node-install #https://gist.github.com/isaacs/579814
     cd ~vagrant/local; 
     curl https://nodejs.org/download/release/latest-v6.x/node-v6.9.4-linux-x64.tar.gz | tar xz --strip-components=1;
     '
-} >>/tmp/vagrant-debug-info-log 2>>&1
+} >>/tmp/vagrant-debug-info-log 2>&1
 
 echo pxt
 {
   runuser - vagrant -c '
     . ~vagrant/.bashrc;
     npm install -g jake typings;
-    mkdir -p ~vagrant/pxt ~vagrant/pxt/pxt-microbit
-    git clone https://github.com/microsoft/pxt ~vagrant/pxt
+    mkdir -p ~vagrant/pxt/pxt ~vagrant/pxt/pxt-microbit
+    git clone https://github.com/microsoft/pxt ~vagrant/pxt/pxt
     git clone https://github.com/microsoft/pxt-microbit ~vagrant/pxt/pxt-microbit
     cd ~vagrant/pxt/pxt;
     npm install; 
@@ -45,7 +53,7 @@ echo pxt
     npm install; 
     nohup pxt serve&;
     '
-} >>/tmp/vagrant-debug-info-log 2>>&1
+} >>/tmp/vagrant-debug-info-log 2>&1
 
 echo 'ssh vagrant@127.0.0.1:2222'
 
