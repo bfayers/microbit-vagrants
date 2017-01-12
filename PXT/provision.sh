e6cf33405990a4f38b162acfcff816bb1639e34e
Owen Brotherwood <OwenBrotherwood@users.noreply.github.com>
2017-01-12 12:29:59 +0100

    Update provision.sh

--- PXT/provision.sh
+++ PXT/provision.sh
@@ -19,9 +19,7 @@ echo initial-apt-get
 echo home-local-node-install #https://gist.github.com/isaacs/579814
 {
   runuser - vagrant -c 'echo export PATH=$HOME/local/bin:$PATH' >> ~vagrant/.bashrc
-  runuser - vagrant -c 'mkdir ~vagrant/local'
-  cd ~vagrant/local
-  runuser - vagrant -c 'curl https://nodejs.org/download/release/latest-v6.x/node-v6.9.4-linux-x64.tar.gz | tar xz --strip-components=1'
+  runuser - vagrant -c 'mkdir ~vagrant/local; cd ~vagrant/local; curl https://nodejs.org/download/release/latest-v6.x/node-v6.9.4-linux-x64.tar.gz | tar xz --strip-components=1'
 }
 
 echo pxt
@@ -29,51 +27,19 @@ echo pxt
   runuser - vagrant -c 'npm install -g jake typings'
   runuser - vagrant -c 'mkdir -p ~vagrant/pxt ~vagrant/pxt/pxt-microbit'
   # Note: there was rm's cause git will not clone into a non empty directory so we need a ~vagrant/pxt/pxt structure as ~vagrant/pxt is used So I # it as "not needed"
-  runuser - vagrant -c 'git clone https://github.com/microsoft/pxt ~vagrant/pxt;git clone https://github.com/microsoft/pxt-microbit ~vagrant/pxt/pxt-microbit' # Note: Peli needs to say ok
+  runuser - vagrant -c 'git clone https://github.com/microsoft/pxt ~vagrant/pxt; git clone https://github.com/microsoft/pxt-microbit ~vagrant/pxt/pxt-microbit' # Note: Peli needs to say ok
   runuser - vagrant -c 'cd ~vagrant/pxt/pxt;npm install; typings install; jake; npm install -g pxt'
   runuser - vagrant -c 'cd ~vagrant/pxt/pxt-microbit; npm install ../pxt; npm install; pxt serve' # Note: Peli needs to say ok
 }
 
 cat <<EOF
 
-sudo pxt target microbit
-
-cd pxt-microbit
-sudo npm install -g pxt
-sudo npm install
-pxt serve
-  
-}
-
-
-
 echo x-apt-get
 {
   apt-get -y install xauth xterm firefox
 }>/tmp/x-apt-get-log
 
-# The following is under investigation
+# What is this for ...
 sudo apt-get --qq -y install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get -y install nodejs
-sudo apt-get -y install firefox
-sudo apt-get -y install libgl1-mesa-glx
-
-cd /home/vagrant/pxt && rm -r * && rm -r .* 
-cd /home/vagrant/pxt && git clone https://github.com/microsoft/pxt .
-git clone https://github.com/microsoft/pxt-microbit
-cd /home/vagrant/pxt
-
-sudo npm install -g jake
-sudo npm install -g typings
-sudo npm install
-sudo typings install
-sudo jake
-sudo npm install -g pxt
-sudo pxt target microbit
-
-cd pxt-microbit
-sudo npm install -g pxt
-sudo npm install
-pxt serve
 EOF
