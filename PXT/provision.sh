5ac92117e1aa1769b6248865a99347c9a0ad659b
Owen Brotherwood <OwenBrotherwood@users.noreply.github.com>
2017-01-12 09:34:41 +0100

    Update provision.sh

--- PXT/provision.sh
+++ PXT/provision.sh
@@ -2,12 +2,28 @@
 #
 # This script is run by Vagrant when a new machine is provisioned.
 #
+echo debug-info-log
+{
+  whoami
+  who am i
+  set
+} >/tmp/debug-info-log
+
 echo initial-apt-get 
 {
   sudo apt-get update
   sudo apt-get -y install xauth xterm git build-essential  
 # sudo apt-get -y dist-upgrade #TODO:Solve grub problem http://askubuntu.com/questions/325872/ubuntu-unattended-apt-get-upgrade-grub-install-dialog
-}>/tmp/initial-apt-get-log
+}>/tmp/initial-apt-get-log #2>&1
+
+echo home-local-node-install #https://gist.github.com/isaacs/579814
+sudo -u vagrant  echo 'export PATH=$HOME/local/bin:$PATH' >> ~vagrant/.bashrc
+sudo -u vagrant  . ~vagrant/.bashrc
+sudo -u vagrant  mkdir ~vagrant/local
+cd ~vagrant/local
+sudo -u vagrant curl https://nodejs.org/download/release/latest-v6.x/node-v6.9.4-linux-x64.tar.gz | tar xz --strip-components=1 
+sudo -u vagrant node --version
+sudo -u vagrant npm --version
 
 cat <<EOF
 # The following is under investigation
